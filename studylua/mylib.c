9861e35ab1a762774d0604f7859f83c87329e0ab
ffashion <1323579954@qq.com>
2021-12-02 15:58:52 +0800

    update mylib.c

--- studylua/mylib.c
+++ studylua/mylib.c
@@ -3,22 +3,22 @@
 #include <lua5.3/lualib.h>
 #include <lua5.3/lauxlib.h>
 
-// int add(lua_State *L){
-//     lua_Number one ,two;
-//     //找到lua 入栈的参数
-//     one = luaL_checknumber(L,1);
-//     two = luaL_checknumber(L,2);
-//     //返回值入栈
-//     lua_pushnumber(L,one+two);
-
-
-//     return 1;
-// }
-
-
-int add(int x,int y){
-    return x + y;
+int add(lua_State *L){
+    lua_Number one ,two;
+    //找到lua 入栈的参数
+    one = luaL_checknumber(L,1);
+    two = luaL_checknumber(L,2);
+    
+    //返回值入 lua栈
+    lua_pushnumber(L,one+two);
+    //指定lua 的返回值数量 也就是你调用了多少次 lua_pushnumber
+    return 1;
 }
+
+
+// int add(int x,int y){
+//     return x + y;
+// }
 
 int luaopen_mylib(lua_State* L){
     
